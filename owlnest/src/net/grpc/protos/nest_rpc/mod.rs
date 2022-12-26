70e796296df822c8ff6384035f00366f03d5aa83
DrHuangMHT <DrHuangMHT@outlook.com>
2022-12-26 20:08:39 +0800

    implement nest_rpc (pending)

--- owlnest/src/net/grpc/protos/nest_rpc/mod.rs
+++ owlnest/src/net/grpc/protos/nest_rpc/mod.rs
@@ -1,21 +1,22 @@
 use tonic::{Request, Response, Status};
 use tracing::info;
+pub mod nest_status;
 
-pub mod nest_rpc {
+pub mod generated {
     tonic::include_proto!("nest_rpc");
 }
 
 #[derive(Debug, Default)]
-pub struct NestRpcServer;
+pub struct RequestHandler;
 
 #[tonic::async_trait]
-impl nest_rpc::nest_rpc_server::NestRpc for NestRpcServer {
+impl generated::nest_rpc_server::NestRpc for RequestHandler {
     async fn hb(
         &self,
-        request: Request<nest_rpc::HbRequest>,
-    ) -> Result<Response<nest_rpc::HbReply>, Status> {
+        request: Request<generated::HbRequest>,
+    ) -> Result<Response<generated::HbReply>, Status> {
         info!("Got Hb request: {:?}", request);
-        let reply = nest_rpc::HbReply {
+        let reply = generated::HbReply {
             rand: rand::random::<i64>(),
         };
         Ok(Response::new(reply))
--- owlnest/src/net/grpc/server.rs
+++ owlnest/src/net/grpc/server.rs
@@ -1,20 +1,22 @@
+use libp2p::swarm::NetworkBehaviour;
 use tonic::transport::{Server, Identity, ServerTlsConfig};
 
 use tokio::{sync::mpsc::Sender, task::JoinHandle};
 use tracing::{error, info};
 
-use super::protos::helloworld::{hello_world::greeter_server::GreeterServer, MyGreeter};
+use crate::net::grpc::protos::nest_rpc::RequestHandler;
+use super::protos::nest_rpc::generated::nest_rpc_server::NestRpcServer;
 
 pub async fn startup(addr: String, register: Sender<String>,ident:Identity) -> JoinHandle<()> {
     tokio::spawn(async move {
         register.send(format!("tcp_sock {}", &addr)).await.unwrap();
         drop(register);//Drop it because it's no longer needed
         let addr = addr.parse().unwrap();
-        let greeter = MyGreeter::default();
+        let greeter = RequestHandler::default();
         info!("gRPC server started on {:?}",addr);
         if let Err(e) = Server::builder()
             .tls_config(ServerTlsConfig::new().identity(ident)).unwrap()
-            .add_service(GreeterServer::new(greeter))
+            .add_service(NestRpcServer::new(greeter))
             .serve(addr)
             .await
         {
